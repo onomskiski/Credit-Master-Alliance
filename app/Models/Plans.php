9db8c6ab9fac6e42f1c6478a8ea12250e891481a
arewageek <arewageek@gmail.com>
2023-10-27 15:46:21 +0100

    Merge

--- app/Models/Plans.php
+++ app/Models/Plans.php
@@ -13,8 +13,8 @@ class Plans extends Model
         'name',
         'max_deposit',
         'min_deposit',
-        'min-return',
         'max_return',
+        'min_return',
         'gift_bonus',
         'duration'
     ];
