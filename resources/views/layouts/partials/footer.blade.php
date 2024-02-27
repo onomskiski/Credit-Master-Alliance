40425970b45f9f36a2dbb3c0ffa3679bb52906ff
arewageek <arewageek@gmail.com>
2024-02-27 02:24:06 +0100

    Completed the index

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id=layout-footer>
     <footer class=footer>
-        {{-- <section class=footer__main>
+        <section class=footer__main>
             <div class=uk-container>
                 <div uk-grid="">
                     <div class="uk-flex uk-width-auto@s uk-width1-1"><a href="https_/247cointrading.com/index.html"><img alt="{{ env('APP_NAME') }}" class=lazyload data-src="resources/1/img/logo.png" height=40 width=136> </a> </div>
@@ -61,7 +61,7 @@
                     </div>
                 </div>
             </div>
-        </section> --}}
+        </section>
         <section class=footer__navbar>
             <div class=uk-container>
                 <div class="uk-child-width-auto@s uk-flex-between uk-flex-middle" uk-grid="">
@@ -215,3 +215,181 @@
         <path d="M0 12h96v12H0zM0 42h96v12H0zM0 72h96v12H0z" fill=currentColor></path>
     </symbol>
 </svg>
+
+
+
+{{-- <div class='w-full py-5 px-4 md:px-8 bg-sky-700 shadow-lg text-sky-50 text-md border-t-2 border-t-gray-300'>
+    <div class='flex flex-col space-y-5'>
+        
+        <div class='w-full flex justify-center flex-col md:flex-row md:space-x-10'>
+            <div class='px-3 md:px-7 py-3'>
+                <h3 class='font-bold text-2xl mb-3s text-white uppercase'>
+                    who are we
+                </h3>
+                <div class='capitalize'>
+                    <div class='py-2'>
+                        <a href="/about-us">
+                            About Us
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/affiliate">
+                            affiliate
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/career">
+                            career
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/contact-us">
+                            contact us
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class='px-3 md:px-7 py-3'>
+                <h3 class='font-bold text-2xl mb-3s text-white uppercase'>
+                    how it works
+                </h3>
+                <div class='capitalize'>
+                    <div class='py-2'>
+                        <a href="/credit-repair-process">
+                            credit repair process
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/what-you-get">
+                            what you get
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/career">
+                            career
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/frequently-asked-questions">
+                            faq
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class='px-3 md:px-7 py-3'>
+                <h3 class='font-bold text-2xl mb-3s text-white uppercase'>
+                    reviews
+                </h3>
+                <div class='capitalize'>
+                    <div class='py-2'>
+                        <a href="/customer-reviews">
+                            customer reviews
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/youtube">
+                            youtube
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/create-a-review">
+                            create a review
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class='px-3 md:px-7 py-3'>
+                <h3 class='font-bold text-2xl mb-3s text-white uppercase'>
+                    sign up today
+                </h3>
+                <div class='capitalize'>
+                    <div class='py-2'>
+                        <a href="/register">
+                            get started
+                        </a>
+                    </div>
+
+                    <div class='py-2'>
+                        <a href="/login">
+                            member login
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class='px-3 md:px-7 py-3'>
+                <h3 class='font-bold text-2xl mb-3s text-white uppercase'>
+                    follow
+                </h3>
+                <div class='flex items-center text-3xl space-x-3'>
+                    <a href="#" className='pr-5'>
+                        <div className='text-sky-50 py-2 text-lg flex justify-start hover:text-white cursor-pointer items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg>
+                        </div>
+                    </a>
+
+                    <a href="#" className='pr-5'>
+                        <div className='text-sky-50 py-2 text-lg flex justify-start hover:text-white cursor-pointer items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+                            </svg>
+                        </div>
+                    </a>
+
+                    <a href="#" className='pr-5'>
+                        <div className='text-sky-50 py-2 text-lg flex justify-start hover:text-white cursor-pointer items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class='border-t-2 border-t-sky-700'>
+            <div class='my-4'>
+                <div class='text-sm text-center'>
+                    <div class='flex space-x-10 justify-center flex-wrap'>
+                        <span class="">
+                            Terms Of Use
+                        </span>
+
+                        <span class="">
+                            Privacy Policy
+                        </span>
+
+                        <span class="">
+                            Privacy Notice
+                        </span>
+
+                        <span class="">
+                            Consumer Privacy Rights
+                        </span>
+
+                        <span class="">
+                            Do not sell or share my personal information
+                        </span>
+                    </div>
+
+                    <div class="font-bold mt-4">
+                        @2024 Credit Master Alliance, LLC.  All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -263,41 +263,150 @@
             </div>
         </div>
 
-        <div class="w-full md:[w-50%] mx-auto bg-sky-700 text-white py-[50pt] md:py-[100pt]">
-            <div>
-                <h3 class="text-4xl font-bold text-center text-white">
-                    Ready to start your credit journey?
-                </h3>
-            </div>
-
-            <div class="flex justify-around w-full">
-                <div class="flex flex-col space-y-3 items-center py-10">
-                    <p class="text-lg">
-                        I've seen my credit, and something isn't right
-                    </p>
-                    <div class="px-6">
-                        <button class="bg-orange-500 text-white px-5 py-3.5">
-                            Get credit help
-                        </button>
+        <div class="w-full bg-sky-700 text-white py-[50pt]">
+            <div class="md:w-[70%] mx-auto md:py-[100pt]">
+                <div>
+                    <h3 class="text-4xl font-bold text-center text-white">
+                        Ready to start your credit journey?
+                    </h3>
+                </div>
+    
+                <div class="flex justify-between flex-col md:flex-row w-full items-center mt-10">
+                    <div class="flex flex-col space-y-3 items-center py-10 w-full">
+                        <p class="text-lg">
+                            I've seen my credit, and something isn't right
+                        </p>
+                        <div class="px-6">
+                            <button class="bg-orange-500 text-white px-8 py-3.5 text-sm font-[450]">
+                                Get credit help
+                            </button>
+                        </div>
                     </div>
-                </div>
-
-                <div class="md:w-2 md:h-full w-[50pt] h-2 bg-sky-300 ">
-
-                </div>
-
-                <div class="flex flex-col space-y-3 items-center py-10">
-                    <p class="text-lg">
-                        I'm not sure what's on my credit
-                    </p>
-                    <div class="px-6">
-                        <button class="bg-orange-500 text-white px-5 py-3.5">
-                            Get a FREE Credit Snapshot
-                        </button>
+    
+                    <div class="flex flex-col space-y-3 items-center py-10 border-t-2 border-t-sky-300 md:border-l-2 md:border-l-sky-300 w-full">
+                        <p class="text-lg">
+                            I'm not sure what's on my credit
+                        </p>
+                        <div class="px-6">
+                            <button class="bg-orange-500 text-white px-8 py-3.5 text-sm font-[450]">
+                                Get a FREE Credit Snapshot
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         
     </section>
+
+
+    <div class="w-full px-5 md:px-[30%] py-[20pt] flex flex-col justify-center items-center">
+        {{-- free credit valuation request --}}
+
+        <div class="w-full text-center">
+            <h2 class="font-[450] text-2xl md:text-4xl text-sky-600 my-4">
+                Get your FREE online credit evaluation now
+            </h2>
+            <div class="text-left px-2 md:px-[30pt] text-2xl flex flex-col space-y-3 md:w-[70%] mx-auto my-4">
+                <div>
+                    <b class="font-[450]">FREE</b> credit score
+                </div>
+                <div>
+                    <b class="font-[450]">FREE</b> negative Item Summary
+                </div>
+                <div>
+                    <b class="font-[450]">FREE</b> recommended Credit Solution
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center w-full">
+            <h2 class="font-[450] text-2xl md:text-4xl text-sky-600 my-4">First, we need some info to pull your credit</h2>
+            <p>
+                Don't worry &mdash; this is secure and won't hurt your score
+            </p>
+            <p>
+                And did we mention it's FREE?
+            </p>
+        </div>
+
+        <div class="flex flex-col justify-center items-center w-full">
+
+            <form>
+                <div class="w-full flex py-3 justify-between flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
+                    <div class="w-full md:w-1/2">
+                        <input type="text" name="fName" placeholder="First Name" class="w-full p-4 bg-gray-50">
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <input type="text" name="lName" placeholder="Last Name" class="w-full p-4 bg-gray-50">
+                    </div>
+                </div>
+
+                <div class="w-full flex py-3 justify-between flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
+                    <div class="w-full md:w-1/2">
+                        <input type="email" name="email" placeholder="Email Address" class="w-full p-4 bg-gray-50">
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <input type="tel" name="tel" placeholder="Phone Number" class="w-full p-4 bg-gray-50">
+                    </div>
+                </div>
+
+                <div class="w-full flex py-3 justify-between flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
+                    <div class="w-full md:w-1/2">
+                        <input type="text" name="street" placeholder="Street Address" class="w-full p-4 bg-gray-50">
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <input type="text" name="zip" placeholder="Zip Code" class="w-full p-4 bg-gray-50">
+                    </div>
+                </div>
+
+                <div class="w-full flex justify-center items-center my-5">
+                    <button type="submit" class="bg-orange-500 px-12 py-3 hover:bg-accent-100/0 border-2 border-orange-500 text-white hover:text-orange-600 transition">
+                        Submit
+                    </button>
+                </div>
+
+                <div>
+                    <p class="text-xs text-gray-800 text-justify">
+                        By clicking 'Submit' I agree by electronic signature to: (1) be contacted by CreditRepair.com about credit repair or credit repair marketing by a live agent, artificial or prerecorded voice, and SMS text at my residential or cellular number, dialed manually or by autodialer even if my phone number is on a do-not-call registry (consent to be contacted is not a condition to purchase services); and (2) the Privacy Policy and Terms of Use (including this arbitration Provision ).
+                    </p>
+                </div>
+            </form>
+        </div>
+
+        
+    </div>
+    
+
+    <div class="w-full flex flex-col md:flex-row bg-sky-700 text-blue-50 my-4 items-center">
+
+        <div class="w-full md:w-1/2 min-h-full">
+          <img src="https://www.creditrepair.com/content/dam/credit-repair/common/assets/imgs/Mask-Group-63.jpg" alt="mm" class="h-[100%] min-h-[400pt] w-full bg-gray-300">
+        </div>
+        <div class=" w-full md:w-1/2 flex justify-center items-center px-3 py-5 md:py-[100pt]">
+          {{-- text-block --}}
+
+            <div class="flex space-y-4 flex-col text-center md:text-left items-center justify-center md:items-start md:justify-start">
+                <div class="flex flex-col md:space-y-2">
+                    <div class="text-xl md:text-3xl font-bold">
+                        CreditMastersAlly.com
+                    </div>
+                    <div class="text-xl md:text-3xl font-bold">
+                        has worked with over
+                    </div>
+                </div>
+                <p class="text-4xl md:text-5xl font-bold pt-10 pb-7 leading-relaxed">
+                    <span class="font-bold text-[50pt] md:text-[55pt]">850,000</span> 
+                    members
+                </p>
+                <p class="text-xl font-bold md:text-3xl">
+                    with their credit since 2012
+                </p>
+            </div>
+        </div>
+        
+    </div>
 @include('layouts.partials.footer')
